f7a61d5901fe75a9da08d7d80813e851fe2a48a5
Abdi036 <abdikumela036@gmail.com>
2025-08-07 04:29:48 -1200

    integrated a chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from "./_context/AuthContext";
 import "./globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Script from "next/script";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -26,6 +27,22 @@ export default function RootLayout({
         className={`${josefin.className} min-h-screen relative bg-gray-900 text-white`}
       >
         <AuthProvider>{children}</AuthProvider>
+        <Script id="chatbase-embed" strategy="afterInteractive">
+          {`
+            window.embeddedChatbotConfig = {
+              chatbotId: "X4GJ0auDjuNqRNvgc0vaj",
+            };
+            (function() {
+              var s = document.createElement("script");
+              s.src = "https://www.chatbase.co/embed.min.js";
+              s.async = true;
+              s.onload = function() {
+                // Chatbot script loaded
+              };
+              document.body.appendChild(s);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
